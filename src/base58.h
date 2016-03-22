22ae2238eaada9d680440acde5ac854e543b1a9c
vergecurrency <vergecurrency@hushmail.com>
2016-03-22 06:58:45 -0400

    Revert "Revert "stealth address""
    This reverts commit ed5e00d035dfa029bf2102aee4af1000a7dc106d.

--- src/base58.h
+++ src/base58.h
@@ -20,6 +20,7 @@
 #include "bignum.h"
 #include "key.h"
 #include "script.h"
+#include "stealth.h"
 
 static const char* pszBase58 = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
 
@@ -269,6 +270,7 @@ public:
     bool operator()(const CKeyID &id) const;
     bool operator()(const CScriptID &id) const;
     bool operator()(const CNoDestination &no) const;
+	bool operator()(const CStealthAddress &stxAddr) const { return false; }
 };
 
 class CBitcoinAddress : public CBase58Data
@@ -397,6 +399,7 @@ public:
 bool inline CBitcoinAddressVisitor::operator()(const CKeyID &id) const         { return addr->Set(id); }
 bool inline CBitcoinAddressVisitor::operator()(const CScriptID &id) const      { return addr->Set(id); }
 bool inline CBitcoinAddressVisitor::operator()(const CNoDestination &id) const { return false; }
+bool inline CBitcoinAddressVisitor::operator()(const CStealthAddress &stxAddr) const      { return false; }
 
 /** A base58-encoded secret key */
 class CBitcoinSecret : public CBase58Data
